96b65e8b5a432a29cfc30ca833ae174dea384ef6
Nakul Bharti <knakul853@gmail.com>
2019-01-16 00:50:40 +0530

    Create phi.cpp

---
+++ seive/phi.cpp
@@ -0,0 +1,22 @@
+std::vector <int> prime;
+bool is_composite[MAXN];
+int phi[MAXN];
+
+void sieve (int n) {
+	std::fill (is_composite, is_composite + n, false);
+	for (int i = 2; i < n; ++i) {
+		if (!is_composite[i]) {
+			prime.push_back (i);
+			phi[i] = i - 1;					//i is prime
+		}
+		for (int j = 0; j < prime.size (); ++j) {
+			is_composite[i * prime[j]] = true;
+			if (i % prime[j] == 0) {
+				phi[i * prime[j]] = phi[i] * prime[j];	//prime[j] divides i
+				break;
+			} else {
+				phi[i * prime[j]] = phi[i] * phi[prime[j]];	//prime[j] does not divide i
+			}
+		}
+	}
+}
