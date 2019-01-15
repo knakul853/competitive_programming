e32817835a5bacbbd93541a8d75c2d7f81a00df1
Nakul Bharti <knakul853@gmail.com>
2019-01-15 17:48:48 +0530

    Create linearsieve.cpp

---
+++ seive/linearsieve.cpp
@@ -0,0 +1,14 @@
+vector <int> prime;
+bool is_composite[MAXN];
+
+void sieve (int n) {
+	std::fill (is_composite, is_composite + n, false);
+	for (int i = 2; i < n; ++i) {
+		if (!is_composite[i]) prime.push_back (i);
+		for (int j = 0; j < prime.size () && i * prime[j] < n; ++j) {
+			is_composite[i * prime[j]] = true;
+			if (i % prime[j] == 0) break;
+		}
+	}
+}
+
