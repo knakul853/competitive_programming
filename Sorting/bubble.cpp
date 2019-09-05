f8daa8b940bab523d08201d67260fa1deb84aa04
Nakul Bharti <knakul853@gmail.com>
2019-09-05 22:52:07 +0530

    Create bubble.cpp

---
+++ Sorting/bubble.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+
+void bubble_sort(int a[],int n)
+{
+	for(int pass = 1; pass<=n-1;pass++)
+	{
+		for(int i=1;i<=n-pass;i++)
+		{
+			if(a[i] > a[i+1])
+			swap(a[i] , a[i+1]);
+		}
+	}
+}
+
+void print()
+{
+	for(int i=1;i<=n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+}
+
+int main()
+{
+	int n;
+
+	cin >> n;
+	
+	int a[n];
+	for(int i=1;i<=n;i++)
+	{
+		cin >> a[i];
+	}
+    bubble_sort(a,n);
+	print();
+
+}
