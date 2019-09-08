dfe4ece3a8be3cc3791c19cb0ae8f007116bbda2
Nakul Bharti <knakul853@gmail.com>
2019-09-09 01:06:01 +0530

    Update Articulation point.cpp

--- Graph/Articulation point.cpp
+++ Graph/Articulation point.cpp
@@ -5,3 +5,6 @@ problems ans solutions:
   
 2.  // http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=13&page=show_problem&problem=1140
   //https://pastebin.com/pd84rFJd
+    
+3.  //http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=5&page=show_problem&problem=251
+    //https://pastebin.com/aGpytp0R
