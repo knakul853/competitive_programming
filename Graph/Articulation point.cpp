fbb81d483919ffebc9d2e7a588a9d6ac1e26cc80
Nakul Bharti <knakul853@gmail.com>
2019-09-09 00:32:59 +0530

    Update Articulation point.cpp

--- Graph/Articulation point.cpp
+++ Graph/Articulation point.cpp
@@ -1,2 +1,7 @@
-//Submerging Islands : https://www.spoj.com/problems/SUBMERGE/
-//https://pastebin.com/mDkNz7zW
+problems ans solutions:
+
+1.  //Submerging Islands : https://www.spoj.com/problems/SUBMERGE/
+    //https://pastebin.com/mDkNz7zW
+  
+2.  // http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=13&page=show_problem&problem=1140
+  //https://pastebin.com/pd84rFJd
