14671e8bc9b9e3f853da4abdb693a7981c12d04d
Nakul Bharti <knakul853@gmail.com>
2019-09-09 01:08:41 +0530

    Update Articulation point.cpp

--- Graph/Articulation point.cpp
+++ Graph/Articulation point.cpp
@@ -1,3 +1,24 @@
+void dfs(int v, int p = -1) {
+    visited[v] = true;
+    tin[v] = low[v] = timer++;
+    int children=0;
+    for (int to : adj[v]) {
+        if (to == p) continue;
+        if (visited[to]) {
+            low[v] = min(low[v], tin[to]); 
+        } else {
+            dfs(to, v);
+            low[v] = min(low[v], low[to]);
+            if (low[to] >= tin[v] && p!=-1)     // there is not present any back edge from v to its ancester
+                IS_CUTPOINT(v);
+            ++children;
+        }
+    }
+    if(p == -1 && children > 1)     /// root is the articualtion point
+        IS_CUTPOINT(v);
+}
+
+
 problems ans solutions:
 
 1.  //Submerging Islands : https://www.spoj.com/problems/SUBMERGE/
