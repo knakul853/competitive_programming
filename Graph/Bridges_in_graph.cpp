dd0f0bbd66af9d3b3f4f6c3f09c8d831645e3a61
Nakul Bharti <knakul853@gmail.com>
2019-09-09 15:44:31 +0530

    Create Bridges_in_graph.cpp

---
+++ Graph/Bridges_in_graph.cpp
@@ -0,0 +1,30 @@
+void dfs(int v,int p = -1)
+{
+   visit[v] = 1;
+    tmin[v] = low[v] = t++;
+    int child = 0;
+    for(auto u:g[v])
+    {
+        if(p==u)continue;
+        if(visit[u])
+        {
+            low[v] = min(low[v] , tmin[u]);
+        }
+        else
+        {
+              dfs(u,v);
+              low[v] = min(low[v] , low[u]);
+              if(tmin[v] < low[u])   // v --> u is bridge iff it is not back edges 
+              {  int a  = u,b= v;
+                if(a>b)swap(a,b);
+                  ans.insert({a,b});
+                  
+            }
+        }
+    }
+   
+}
+
+problems and solutions :
+/// http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=737
+// https://pastebin.com/69d9xW47
