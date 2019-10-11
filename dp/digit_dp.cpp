566ffaf714feb7be1ec355305c4b505bcd7b0258
Nakul Bharti <knakul853@gmail.com>
2019-10-12 01:37:39 +0530

    Added some Spoj digit dp questions

--- dp/digit_dp.cpp
+++ dp/digit_dp.cpp
@@ -6,3 +6,12 @@ https://pastebin.com/S0V87pUJ
 
 https://vjudge.net/problem/LightOJ-1205
 https://pastebin.com/tLpJM5R3
+
+https://www.spoj.com/problems/RAONE/
+https://pastebin.com/3kycMuSP
+
+https://www.spoj.com/problems/LUCIFER/
+https://pastebin.com/U6frDGRr
+
+https://www.spoj.com/problems/GONE/
+https://pastebin.com/bfR7HXH7
