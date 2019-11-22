ff3ead6d50b8d7051651e7bd3fe20906c0521ff1
Nakul Bharti <knakul853@gmail.com>
2019-11-23 02:23:06 +0530

    Update Query_on_tree.cpp

--- Tree/Query_on_tree.cpp
+++ Tree/Query_on_tree.cpp
@@ -4,8 +4,20 @@ https://www.codechef.com/viewsolution/27793430
 http://acm.timus.ru/problem.aspx?space=1&num=1471
 https://pastebin.com/ZgrNbPPa
 
-https://codeforces.com/problemset/problem/342/E
+
+
+Centroid Decomposition
+
+https://codeforces.com/problemset/problem/342/E   : https://codeforces.com/contest/342/submission/65504883
+
+https://www.spoj.com/problems/QTREE5/             : https://pastebin.com/R4LYKqpH
 
 
 todo : 
-https://www.spoj.com/problems/QTREE5/      https://pastebin.com/rkTEQbjM
+http://www.codechef.com/problems/PRIMEDST
+https://www.hackerrank.com/challenges/bst-maintenance
+http://www.spoj.com/problems/QTREE4/
+http://codeforces.com/gym/100570/problem/F
+http://www.ioi2011.or.th/hsc/tasks/EN/race.pdf
+http://www.codechef.com/problems/TESTERS
+http://codeforces.com/contest/321/problem/C
