1c7d02d80d4886a7776204afcfe0b02c09db5470
Nakul Bharti <knakul853@gmail.com>
2019-11-22 17:49:58 +0530

    Update Query_on_tree.cpp

--- Tree/Query_on_tree.cpp
+++ Tree/Query_on_tree.cpp
@@ -4,6 +4,8 @@ https://www.codechef.com/viewsolution/27793430
 http://acm.timus.ru/problem.aspx?space=1&num=1471
 https://pastebin.com/ZgrNbPPa
 
+https://codeforces.com/problemset/problem/342/E
+
 
 todo : 
-https://codeforces.com/problemset/problem/342/E
+https://www.spoj.com/problems/QTREE5/      https://pastebin.com/rkTEQbjM
